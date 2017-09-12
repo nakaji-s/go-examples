bea1fefdac51d45fdb7ff621dac0ca7031e6e442
nakaji-s <nakajima.sota.sub@gmail.com>
2017-09-12 22:33:50 +0900

    Add rotate example

--- logging/gommon/main.go
+++ logging/gommon/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"github.com/labstack/gommon/log"
 	"os"
+	"time"
+
+	"github.com/labstack/gommon/log"
+	"github.com/lestrrat/go-file-rotatelogs"
 )
 
 func main() {
@@ -14,7 +17,21 @@ func main() {
 	// msg1
 	log.Error("example error message1")
 
-	// msg2
+	// msg2(set header)
 	log.SetHeader("--------")
 	log.Error("example error message2")
+
+	// msg3(file with rotate)
+	logf, err := rotatelogs.New(
+		"./log.%Y%m%d%H%M",
+		rotatelogs.WithLinkName("./log"),
+		rotatelogs.WithMaxAge(24*time.Hour),
+		rotatelogs.WithRotationTime(time.Hour),
+	)
+	if err != nil {
+		log.Printf("failed to create rotatelogs: %s", err)
+		return
+	}
+	log.SetOutput(logf)
+	log.Error("example error message3")
 }
