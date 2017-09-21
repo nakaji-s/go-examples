1bb90d3c69adaaaf35b8e511f5f0a2c08b3fdf99
nakaji-s <nakajima.sota.sub@gmail.com>
2017-09-22 01:58:57 +0900

    Add original middleware for dump request to echo

--- server/echo/main.go
+++ server/echo/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 
 	"io/ioutil"
+
+	"fmt"
 
 	"github.com/labstack/echo"
 )
@@ -11,6 +14,25 @@ import (
 func main() {
 	// start server
 	e := echo.New()
+
+	// original middleware for dump queryParam,header,body
+	e.Use(
+		func(next echo.HandlerFunc) echo.HandlerFunc {
+			return func(c echo.Context) error {
+				fmt.Println("queryParam:", c.QueryParams())
+				fmt.Println("header:", c.Request().Header)
+				body, err := ioutil.ReadAll(c.Request().Body)
+				if err != nil {
+					return err
+				}
+				c.Request().Body = ioutil.NopCloser(bytes.NewBuffer(body)) // Reset
+				fmt.Println("body", string(body))
+
+				err = next(c)
+				return err
+			}
+		},
+	)
 
 	e.GET("/hello", func(c echo.Context) error {
 		return c.String(http.StatusOK, "hello")
