4add4cd6ab5650bf5adc06c56ef48778061fce36
nakaji-s <nakajima.sota.sub@gmail.com>
2017-09-27 23:03:45 +0900

    Add vue-router example without build system

--- server/echo/static/script.js
+++ server/echo/static/script.js
@@ -1,11 +1,34 @@
 // ここはVueそのまま
 var app = new Vue({
     el: "#app",
-    data: {
-        todos: [
-            { name: "one" },
-            { name: "two" },
-            { name: "three" }
+    data: function() {
+        return {
+            message: 'Hello World!'
+        }
+    },
+    router: new VueRouter({
+        // 各ルートにコンポーネントをマッピングします
+        // コンポーネントはVue.extend() によって作られたコンポーネントコンストラクタでも
+        // コンポーネントオプションのオブジェクトでも構いません
+        routes: [
+            {
+                path: '/top',
+                component: {
+                    template: '<div>トップページです。</div>'
+                }
+            },
+            {
+                path: '/users',
+                component: {
+                    template: '<div>ユーザー一覧ページです。</div>'
+                }
+            },
+            {
+                path: '/todo',
+                component: {
+                    template: '#todo-item'
+                }
+            }
         ]
-    }
-});
+    })
+}).$mount('#app');
