e0f366ecbe457da2971e6269a9e5ef31cdbbc360
nakaji-s <nakajima.sota.sub@gmail.com>
2017-10-20 02:36:16 +0900

    tmp

--- orm/gorm/main.go
+++ orm/gorm/main.go
@@ -1,14 +1,49 @@
 package main
 
 import (
+	"fmt"
+
+	"strings"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/kr/pretty"
 )
 
 type Product struct {
 	gorm.Model
 	Code  string
 	Price uint
+	Raw   interface{} `gorm:"type:varchar(100)"`
+}
+
+func beforeCreate(scope *gorm.Scope) {
+	//scope.Value
+	field, _ := scope.FieldByName("Raw")
+	pretty.Println(gorm.Field(*field))
+	product := scope.Value.(*Product)
+
+	fmt.Println("--------")
+	pretty.Println(product)
+
+	//scope.GetModelStruct()
+	//scope.Set("price", 10)
+	//scope.SetColumn("price", 3)
+	fmt.Println(fmt.Sprint(product.Raw))
+	err := scope.SetColumn("raw", strings.Join(product.Raw.([]string), ","))
+	if err != nil {
+		panic(err)
+	}
+	pretty.Println(scope.Value)
+
+	/*
+		//pretty.Println(scope.FieldByName("Raw"))
+		reflectValue := reflect.Indirect(reflect.ValueOf(scope.Value))
+		if strings.Contains(string(reflectValue.Type().Field(0).Tag), "uuid") {
+			uuid.SetClockSequence(-1)
+			scope.SetColumn("id", uuid.NewUUID().String())
+		}
+	*/
 }
 
 func main() {
@@ -17,17 +52,25 @@ func main() {
 		panic("failed to connect database")
 	}
 	defer db.Close()
-
 	db.LogMode(true)
+
+	db.Callback().Create().Before("gorm:create").Register("my_plugin:before_create", beforeCreate)
+
+	var product Product
+	db.DropTableIfExists(&product)
 
 	// Migrate the schema
 	db.AutoMigrate(&Product{})
 
 	// Create
-	db.Create(&Product{Code: "L1212", Price: 1000})
+	//b := []byte([]string{"aaa"})
+	//err = db.Create(&Product{Code: "L1212", Price: 1000}).Error
+	err = db.Create(&Product{Code: "L1212", Price: 1000, Raw: []string{"aaa", "bbb"}}).Error
+	if err != nil {
+		panic(err)
+	}
 
 	// Read
-	var product Product
 	db.First(&product, 1)                   // find product with id 1
 	db.First(&product, "code = ?", "L1212") // find product with code l1212
 
