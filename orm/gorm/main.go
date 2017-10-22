e7a99cd7416dc256154e1f9492d6735a16051e55
nakaji-s <nakajima.sota.sub@gmail.com>
2017-10-22 23:43:07 +0900

    Add example for sql.Scanner to gorm

--- orm/gorm/main.go
+++ orm/gorm/main.go
@@ -3,15 +3,50 @@ package main
 import (
 	"strings"
 
+	"fmt"
+
+	"database/sql/driver"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	//_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/kr/pretty"
 )
 
 type Product struct {
 	gorm.Model
 	Code  string
 	Price uint
-	Raw   interface{} `gorm:"type:varchar(100)"`
+	Raw   interface{}   `gorm:"type:varchar(100)"`
+	Str   MyStringArray `gorm:"type:varchar(64)"`
+}
+
+type MyStringArray []string
+
+func (a *MyStringArray) Scan(src interface{}) error {
+	switch src := src.(type) {
+	case []byte:
+		*a = strings.Split(string(src), `,`)
+		return nil
+	case string:
+		*a = strings.Split(src, `,`)
+		return nil
+	case nil:
+		*a = nil
+		return nil
+	}
+
+	return fmt.Errorf("cannot convert %T to MyStringArray", src)
+}
+
+func (a MyStringArray) Value() (driver.Value, error) {
+	if a == nil {
+		return nil, nil
+	}
+
+	ret := fmt.Sprint(strings.Join(a, `,`))
+
+	return ret, nil
 }
 
 func beforeCreate(scope *gorm.Scope) {
@@ -26,6 +61,7 @@ func beforeCreate(scope *gorm.Scope) {
 
 func main() {
 	db, err := gorm.Open("sqlite3", "test.db")
+	//db, err := gorm.Open("postgres", "host=localhost user=postgres sslmode=disable password=admin")
 	if err != nil {
 		panic("failed to connect database")
 	}
@@ -41,7 +77,7 @@ func main() {
 	db.AutoMigrate(&Product{})
 
 	// Create
-	err = db.Create(&Product{Code: "L1212", Price: 1000, Raw: []string{"aaa", "bbb"}}).Error
+	err = db.Create(&Product{Code: "L1212", Price: 1000, Raw: []string{"aaa", "bbb"}, Str: []string{"aaa", "bbb"}}).Error
 	if err != nil {
 		panic(err)
 	}
@@ -49,6 +85,7 @@ func main() {
 	// Read
 	db.First(&product, 1)                   // find product with id 1
 	db.First(&product, "code = ?", "L1212") // find product with code l1212
+	pretty.Println(product)
 
 	// Update - update product's price to 2000
 	db.Model(&product).Update("Price", 2000)
