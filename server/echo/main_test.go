5bdd433150d177e135b21759e627e5a3c86a3297
nakaji-s <nakajima.sota.sub@gmail.com>
2017-10-23 23:17:11 +0900

    Add httpexpect example

--- server/echo/main_test.go
+++ server/echo/main_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"net/http"
 	"os"
 	"testing"
 
+	"github.com/gavv/httpexpect"
 	"github.com/go-resty/resty"
 	"github.com/stretchr/testify/assert"
 )
@@ -20,6 +22,15 @@ func TestHello(t *testing.T) {
 	resp, err := resty.R().Get("http://localhost:8081/hello")
 	assert.Nil(t, err)
 	assert.Equal(t, "hello", string(resp.Body()))
+}
+
+func TestHelloId(t *testing.T) {
+	e := httpexpect.New(t, "http://localhost:8081")
+
+	// is it working?
+	e.GET("/hello/" + "me").
+		Expect().
+		Status(http.StatusOK).Body().Equal("hello me")
 }
 
 func TestEcho(t *testing.T) {
